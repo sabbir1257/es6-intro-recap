cac7ac193f19b48855d7403a00b8afa55d52d705
sabbir1257 <masabbir1257@gmail.com>
2024-10-19 21:43:59 +0600

    Template string, multiple line string, dynamic string

---
+++ template.js
@@ -0,0 +1,15 @@
+const first = 'jann';
+const last= 'pakhi';
+const name = first + ' ' + last;
+console.log(name);
+
+const a  = 10;
+const b = 20;
+const result = 'The sum of ' + a + 'and' + b + ' is ' + (a + b);
+// console.log(result);
+
+const numbers = [45, 48, 89, 78];
+const student = {name: 'john ochena', age: 5};
+const  math = `The sum of ${numbers[0]} and ${student.age} is ${numbers[0] + student.age}`;
+console.log(math);
+
