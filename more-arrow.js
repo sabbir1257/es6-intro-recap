4ace9443deee8d0b1eec186df42f6247544709f4
sabbir1257 <masabbir1257@gmail.com>
2024-10-20 13:53:59 +0600

    git push

---
+++ more-arrow.js
@@ -0,0 +1,11 @@
+const difference = (x, y) => x- y;
+const multiply = (first , second, third) => first * second * third;
+
+const getAge = (person) => person.age;
+const student = {name: 'sakib', age: 50};
+const age = getAge(student);
+console.log(age);
+ 
+const  getThird = numbers => numbers[2];
+const third = getThird([5, 6, 88, 2, 13]);
+console.log(third);
