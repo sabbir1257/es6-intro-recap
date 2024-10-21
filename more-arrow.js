e8efd5f4eb29eccdf9bc04c8f5274f9d889ffd0a
sabbir1257 <masabbir1257@gmail.com>
2024-10-21 13:51:20 +0600

    More Arrow functions and big arrow function

--- more-arrow.js
+++ more-arrow.js
@@ -1,6 +1,7 @@
 const difference = (x, y) => x- y;
 const multiply = (first , second, third) => first * second * third;
 
+// single paramwter or one parameter
 const getAge = (person) => person.age;
 const student = {name: 'sakib', age: 50};
 const age = getAge(student);
@@ -9,3 +10,17 @@ console.log(age);
 const  getThird = numbers => numbers[2];
 const third = getThird([5, 6, 88, 2, 13]);
 console.log(third);
+
+const doubleIt = num => num * 2;
+
+// no parameter 
+const getPI = () => Math.PI
+console.log(getPI());
+
+// large arrow function. if you want to get anyting returned from this function. then you have to use the return keyword
+const doMath = (x, y, z) => {
+     const sum = x + y + z;
+     const mult = x * y * z;
+     const result = sum + mult;
+     return result;
+}
