2d54ec447a5b6a8872d0e92c5780762239c31faf
sabbir1257 <masabbir1257@gmail.com>
2024-10-23 23:32:55 +0600

    (advanced) Object and Array Destructure

--- destructuring.js
+++ destructuring.js
@@ -8,11 +8,22 @@ const actor = {
 // use property name as a variable thet contains the property value
 const {phone, age: boyos} = actor;
 
-
 // const phone = actor.phone;
 // const name = actor.name;
 // const age = actor.age;
 
-
 console.log(phone);
 console.log(boyos);
+
+// array destructuring 
+const numbers =[45, 99];
+const [first, second] = numbers;
+const [x, y] = [12, 66];
+
+// advanced
+function douleThem (a, b){
+     return [a*2, b*2]
+};
+
+const [prothom, ditiyo] = douleThem(6, 9);
+console.log(prothom, ditiyo);
