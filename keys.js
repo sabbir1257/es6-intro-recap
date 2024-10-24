89cc13fe381cd43b2d42ff7f3e37cc1688c8d0f9
sabbir1257 <masabbir1257@gmail.com>
2024-10-24 21:57:59 +0600

    Keys, values, entries, delete

---
+++ keys.js
@@ -0,0 +1,24 @@
+const glass = {name: 'glass', color: "golden", price: 12, isCleaned: true};
+
+console.log(glass);
+
+// all properties name
+const keys = Object.keys(glass);
+// [ 'name', 'color', 'price', 'isCleaned' ]
+
+// all property values
+const valuse = Object.values(glass);
+// [ 'glass', 'golden', 12, true ]
+
+const entries = Object.entries(glass);
+// array of array , tow dimensional array
+/* [
+  [ 'name', 'glass' ],
+  [ 'color', 'golden' ],
+  [ 'price', 12 ],
+  [ 'isCleaned', true ]
+] */
+// console.log(entries);
+
+delete glass.isCleaned;
+console.log(glass);
